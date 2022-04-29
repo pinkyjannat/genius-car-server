98d0c139927893daa690e1d6b1c7d040252b9607
Jannatul Ferdous <jannatpinky92@gmail.com>
2022-04-29 04:36:09 -0400

    jwt token added

--- index.js
+++ index.js
@@ -1,14 +1,33 @@
 const express =require('express');
 const cors =require('cors');
+const jwt = require('jsonwebtoken');
 require('dotenv').config();
-const app=express();
 const port =process.env.PORT || 5000;
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 
+const app=express();
 
 //middleware
 app.use(cors());
 app.use(express.json());
+
+function verifyJWT(req,res,next){
+    const authHeader=req.headers.authorization;
+    if(!authHeader){
+        return res.status(401).send({message:'unauthorized access'});
+    }
+    const token =authHeader.split(' ')[1];
+    jwt.verify(token,process.env.ACCESS_TOKEN_SECRET,(err,decoded)=>{
+        if(err){
+            return res.status(403).send({message:'Forbidden access'})
+        }
+        console.log('decoded',decoded);
+        req.decoded=decoded;
+        next();
+    })
+   
+   
+}
 
 
 
@@ -19,6 +38,18 @@ async function run(){
  try{
  await client.connect();
  const serviceCollection=client.db('geniusCar').collection('service');
+ const orderCollection = client.db('geniusCar').collection('order');
+
+
+ //auth
+ app.post('/login',async(req,res)=>{
+  const user=req.body;
+  const accessToken=jwt.sign(user,process.env.ACCESS_TOKEN_SECRET,{expiresIn:'1d'});
+  res.send({accessToken});
+ })
+
+
+ //services api
  app.get('/service',async(req,res)=>{
     const query={};
     const cursor=serviceCollection.find(query);
@@ -48,11 +79,33 @@ async function run(){
   const result=await serviceCollection.deleteOne(query);
   res.send(result);
  });
- }
+ //order collection api
 
- finally{
+ app.get('/order',verifyJWT,async(req,res)=>{
+    const decodedEmail=req.decoded.email;
+     const email=req.query.email;
+    if(email===decodedEmail){
+        const query={email:email};
+        const cursor=orderCollection.find(query);
+        const orders=await cursor.toArray();
+        res.send(orders);
+    }else{
+        res.status(403).send({message:'forbidden access'})
+    }
+ })
+
+
+
+
+app.post('/order',async(req,res)=>{
+const order=req.body;
+const result = await orderCollection.insertOne(order);
+res.send(result);
+})
 
  }
+ finally{}
+ 
 }
 
 run().catch(console.dir);
